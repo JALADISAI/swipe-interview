afa8ca02da744f1d23c7555fe68d01a86f4b3956
saiteja-jaladi <107678434+saiteja-jaladi@users.noreply.github.com>
2023-05-14 16:03:43 +0530

    removed default list

--- src/reducers/invoiceList.reducer.js
+++ src/reducers/invoiceList.reducer.js
@@ -5,39 +5,7 @@ import {
 } from "../types";
 
 const initialState = {
-  list: [
-    {
-      data: {
-        isOpen: false,
-        currency: "$",
-        currentDate: "",
-        invoiceNumber: 1,
-        dateOfIssue: "2023-05-25",
-        billTo: "efsd",
-        billToEmail: "sdg@dfg.com",
-        billToAddress: "sdfgf",
-        billFrom: "dfg",
-        billFromEmail: "dfdf@sdf.com",
-        billFromAddress: "sdf",
-        notes: "Thanks for tying notes",
-        total: "14648.19",
-        subTotal: "12963.00",
-        taxRate: "18",
-        taxAmmount: "2333.34",
-        discountRate: "5",
-        discountAmount: "648.15",
-      },
-      items: [
-        {
-          id: "1",
-          name: "sfg",
-          description: "sdfgs",
-          price: "4321.00",
-          quantity: "3",
-        },
-      ],
-    },
-  ],
+  list: [],
 };
 export default function invoiceListReducer(state = initialState, action) {
   switch (action.type) {
